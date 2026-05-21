05dad334a5904d7379c15595e1b3b78efd32d0f7
pubudini-rathnayake <pubudini-rathnayake-pubudinithamali@gmail.com>
2026-05-21 18:21:59 +0530

    Added SQLite database integration

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
+
+# Create Database and Table
+def init_db():
+
+    conn = sqlite3.connect("database.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS reflections (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            reflection TEXT,
+            mood TEXT,
+            productivity INTEGER
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+# Homepage Route
 @app.route("/", methods=["GET", "POST"])
 def home():
 
@@ -11,11 +34,27 @@ def home():
         mood = request.form["mood"]
         productivity = request.form["productivity"]
 
-        print("Reflection:", reflection)
-        print("Mood:", mood)
-        print("Productivity:", productivity)
+        conn = sqlite3.connect("database.db")
+
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO reflections
+            (reflection, mood, productivity)
+            VALUES (?, ?, ?)
+        """, (reflection, mood, productivity))
+
+        conn.commit()
+        conn.close()
+
+        print("Reflection saved successfully!")
 
     return render_template("index.html")
 
+
+# Run App
 if __name__ == "__main__":
+
+    init_db()
+
     app.run(debug=True)
