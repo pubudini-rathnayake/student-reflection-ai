94d1448de6a00c46911ac7be154c9f34a748e34d
pubudini-rathnayake <pubudini-rathnayake-pubudinithamali@gmail.com>
2026-05-21 18:07:41 +0530

    Connected frontend form to Flask backend

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+
+        reflection = request.form["reflection"]
+        mood = request.form["mood"]
+        productivity = request.form["productivity"]
+
+        print("Reflection:", reflection)
+        print("Mood:", mood)
+        print("Productivity:", productivity)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
